809875f3a077ca70b11fa727ba5dea16537975d7
SweetBunny123 <your_new_email@example.com>
2024-11-24 21:21:45 +0530

    DEPLOY KARENGE

--- World_Wise_Backend/main.py
+++ World_Wise_Backend/main.py
@@ -50,7 +50,7 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://world-wise-mu-ten.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
